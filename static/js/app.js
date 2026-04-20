a22462b0b0a2acd3013bd22aa63678e47501d0ea
Don Macaron <donmacaron95@gmail.com>
2026-04-20 17:07:12 +0500

    Preloader animation added

--- static/js/app.js
+++ static/js/app.js
@@ -6,3 +6,12 @@ var rollBtn = document.getElementById("rollBtn");
 if (rollBtn) rollBtn.classList.add('unlock-3');
 var menuRollBtn = document.querySelector('.menu-btn.roll-btn');
 if (menuRollBtn) menuRollBtn.classList.add('unlock-3');
+
+var preloader = document.getElementById("preloader");
+if (preloader) {
+  requestAnimationFrame(function() {
+    requestAnimationFrame(function() {
+      preloader.classList.add("is-hidden");
+    });
+  });
+}
